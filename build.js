cd524649ae6953082178a44c9744f43dadab41c0
jeffbhove <jeff.bhove@gmail.com>
2022-03-05 12:09:15 -0500

    Fixed issue with registering items specific to pointer types.

--- build.js
+++ build.js
@@ -4,18 +4,28 @@ const { fileHeader, formattedVariables } = StyleDictionary.formatHelpers;
 
 const pointerTypeArr = ['coarse', 'fine'];
 const filesArr = [];
+const transformsArr = ['attribute/cti', 'name/cti/kebab'];
 
 pointerTypeArr.forEach((pointerType) => {
+  /**
+   * If a token has an attribute where 'media' is equal to 'pointer',
+   * and the token name contains the current pointer type,
+   * add a new attribute to the token where 'pointer' is the
+   * current pointer type.
+   *
+   *  "controlMinTarget": {
+   *    "coarse": {
+   *      "value": "{base.size.48}",
+   *        "attributes": {
+   *          "media": "pointer",
+   *          "pointer": "coarse" // This attribue will be added
+   *        }
+   *      }
+   *    }
+   *  }
+   */
   StyleDictionary.registerTransform({
-    name: 'media/pointer',
-    type: 'name',
-    transformer: function (token) {
-      return token.name.replace(`-${pointerType}`, '');
-    },
-  });
-
-  StyleDictionary.registerTransform({
-    name: 'media/pointerType',
+    name: `media/pointerType/${pointerType}`,
     type: 'attribute',
     transformer: function (token) {
       const originalAttrs = token.attributes || {};
@@ -33,8 +43,27 @@ pointerTypeArr.forEach((pointerType) => {
     },
   });
 
+  transformsArr.push(`media/pointerType/${pointerType}`);
+
+  /**
+   * Remove the pointer type from the token name so that the same
+   * token name is generated for each pointer type.
+   */
+  StyleDictionary.registerTransform({
+    name: `media/pointer/${pointerType}`,
+    type: 'name',
+    transformer: function (token) {
+      return token.name.replace(`-${pointerType}`, '');
+    },
+  });
+
+  transformsArr.push(`media/pointer/${pointerType}`);
+
+  /**
+   * Create a format to use for a Media block for pointers.
+   */
   StyleDictionary.registerFormat({
-    name: 'css/media-pointer',
+    name: `css/media-pointer/${pointerType}`,
     formatter: function ({ dictionary, file, options }) {
       const { outputReferences } = options;
       return (
@@ -46,9 +75,14 @@ pointerTypeArr.forEach((pointerType) => {
     },
   });
 
+  /**
+   * Each pointer type should generate it's own file, and will filter the
+   * tokens to only include those that have the pointer attribute matching
+   * the current point type.
+   */
   filesArr.push({
     destination: `build/${pointerType}.css`,
-    format: `css/media-pointer`,
+    format: `css/media-pointer/${pointerType}`,
     filter: (token) => {
       return (
         token.attributes['pointer'] &&
@@ -67,12 +101,7 @@ StyleDictionary.extend({
   platforms: {
     css: {
       transformGroup: `css`,
-      transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'media/pointerType',
-        'media/pointer',
-      ],
+      transforms: transformsArr,
       files: filesArr,
     },
   },
